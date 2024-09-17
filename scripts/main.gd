68a88a2df231cf36d14fe4055e9bf5a0a13598fa
grySal <vim>
2024-09-17 11:48:02 -0700

    deload clickable items

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,18 +1,19 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("main entered")
 	Global.HISTORY = Dialogic.History
 	Global.HISTORY.simple_history_enabled = true
 	Dialogic.signal_event.connect(_on_dialogic_signal)
 	Dialogic.start("res://timelines/main.dtl")
 
 func _on_dialogic_signal(arg):
+	#print(arg)
+	var sceneToLoad = load("res://scenes/%s.tscn" % arg)
+	$".".add_child(sceneToLoad.instantiate())
 	match arg:
 		"clickable_scene_1":
-			print("click 1")
+			print("clsdfasdf")
 		_:
 			print_debug("dialogic signal error")
 	pass
